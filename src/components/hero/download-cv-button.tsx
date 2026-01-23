8a29f107df52db508d9b18ea08a9afd5672f59bf
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2026-01-23 16:17:29 +0200

    feat: add download CV button

--- src/components/hero/download-cv-button.tsx
+++ src/components/hero/download-cv-button.tsx
@@ -2,16 +2,25 @@ import { Download } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 
-const DownloadCvButton = ({}) => (
-    <Button
-        size="lg"
-        variant="outline"
-        className="gap-2 bg-transparent"
-    >
-        {/*todo - download cv*/}
-        <Download className="h-4 w-4"/>
-        Download CV
-    </Button>
-)
+const DownloadCvButton = ({}) => {
+    const handleDownload = () => {
+        const link = document.createElement('a')
+        link.href = '/Bar_Cohen__Full-Stack_developer_CV.pdf'
+        link.download = 'Bar_Cohen_Full-Stack_Developer_CV.pdf'
+        link.click()
+    }
+
+    return (
+        <Button
+            size="lg"
+            variant="outline"
+            className="gap-2 bg-transparent"
+            onClick={handleDownload}
+        >
+            <Download className="h-4 w-4"/>
+            Download CV
+        </Button>
+    )
+}
 
 export default DownloadCvButton
